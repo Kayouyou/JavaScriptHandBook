0e10de89065dd76f7da12752e05ab85f54c886b4
Kayouyou <yeyangyang519716@gmail.com>
2018-01-04 16:25:13 +0800

    js 字符串

--- string.js
+++ string.js
@@ -1,9 +1,24 @@
 //js 字符串
 
 
+//+ 号运算符用于数字，表示两个数字相加，作用于字符串时，字符串相加
 msg = "hello," + "world";
 console.log(msg)
 
 name = "jim";
 greeting = "welcome to my home " + name;
 console.log(greeting)
+
+//字符串长度
+console.log(name.length)
+
+var s = "hello world";
+s.charAt(0)
+s.substring(1,4)
+s.slice(1,4)
+//ell
+console.log(s.substring(1,4))
+
+console.log(s.toUpperCase())
+
+console.log(s[0])
